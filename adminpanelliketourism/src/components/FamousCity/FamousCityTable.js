694ea62c3aa496244f2e930d077f72de8b62b8fa
[umid shamdinli] <[umidash@code.edu.az]>
2022-05-29 17:04:53 +0400

    firsh component creted in table

---
+++ adminpanelliketourism/src/components/FamousCity/FamousCityTable.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function FamousCityTable() {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, consequuntur.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default FamousCityTable
